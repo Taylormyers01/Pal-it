06d16bfac45fa27dcd9c2f68a3f2264d4a197ddf
Tayto <taylormyers01@comcast.net>
2023-08-27 12:59:11 -0400

    Added materials to angular and verified it worked

--- Pal-it2/src/main/webapp/app/app.module.ts
+++ Pal-it2/src/main/webapp/app/app.module.ts
@@ -25,7 +25,6 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
-import { InventoryPaintUpdateComponent } from './entities/inventory-paint/inventory-paint-update/inventory-paint-update.component';
 
 
 @NgModule({
--- Pal-it2/src/main/webapp/app/entities/inventory-paint/inventory-paint.module.ts
+++ Pal-it2/src/main/webapp/app/entities/inventory-paint/inventory-paint.module.ts
@@ -4,9 +4,11 @@ import {InventoryPaintComponent} from "./inventory-paint list/inventory-paint.co
 import {InventoryPaintRoutingModule} from "./inventory-paint route/inventory-paint-routing.module";
 import {InventoryPaintDetailComponent} from "./inventory-paint-detail/intentory-paint-detail.component";
 import {InventoryPaintUpdateComponent} from "./inventory-paint-update/inventory-paint-update.component";
+import {MatCardModule} from '@angular/material/card';
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  imports: [SharedModule, InventoryPaintRoutingModule],
+  imports: [SharedModule, InventoryPaintRoutingModule, MatCardModule, MatButtonModule],
   declarations: [InventoryPaintComponent, InventoryPaintDetailComponent, InventoryPaintUpdateComponent],
 })
 export class InventoryPaintModule {}
