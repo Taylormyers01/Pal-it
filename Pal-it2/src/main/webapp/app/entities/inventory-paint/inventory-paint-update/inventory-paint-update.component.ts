6af7025c932964efdcb60b2087f9fbadd0492136
taylor <taylormyers01@comcast.net>
2023-08-28 13:32:34 -0400

    adding recent changes

--- Pal-it2/src/main/webapp/app/entities/inventory-paint/inventory-paint-update/inventory-paint-update.component.ts
+++ Pal-it2/src/main/webapp/app/entities/inventory-paint/inventory-paint-update/inventory-paint-update.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PaintService} from "../../paint/service/paint.service";
 import {IApplicationUser} from "../../application-user/application-user.model";
+import {IPaint} from "../../paint/paint.model";
+import {ApplicationUserService} from "../../application-user/service/application-user.service";
 
 
 @Component({
@@ -10,17 +12,24 @@ import {IApplicationUser} from "../../application-user/application-user.model";
 })
 export class InventoryPaintUpdateComponent implements OnInit {
   applicationUser: IApplicationUser | null = null;
-
+  ownedPaints: IPaint[] = [];
+  availablePaints: IPaint[] | null = [];
 
   constructor(
     protected activatedRoute: ActivatedRoute,
     protected paintService: PaintService,
+    protected applicationUserService: ApplicationUserService,
     ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ applicationUser }) => {
+    this.activatedRoute.data.subscribe(({applicationUser}) => {
       this.applicationUser = applicationUser;
     });
+    if(this.applicationUser) {
+      this.ownedPaints = this.applicationUser.ownedPaints ?? [];
+    }
+    this.paintService.query().subscribe(data => this.availablePaints = data.body);
   }
+
 
 }
